f9758f32a971afb8a5746db8dcb524187cb8c087
Sonal Mittal <sonalmittal140103@gmail.com>
2025-01-28 16:31:57 +0530

    Arthematic Operator

--- Instructions and Operators/variables.c
+++ Instructions and Operators/variables.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
 int main(){
-    int i;
-    int j = 10;
-    printf("The value of i is %d and the value if j is %d.", i,j);
+    int i = 10;
+    int j = i;
+    printf("The value of i is %d and the value if j is %d.\n ", i,j);
+
+    int a = 3;
+    int b = 6;
+    int c = a+b;
+    printf("The value of a is %d.\nThe value of b is %d.\nAnd their Sum is : %d.\n",a,b,c);
+    printf("The remainder when a is divided by b is : %d\n", a%b);
+
+    // Use pow() for exponentiation
+    double d = pow(b, a); // pow() returns a double, so use double or cast to int
+    printf("The value of b raised to the power a is: %.2f\n", d);
+
+    
     return 0;
 }
