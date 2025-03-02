271feed8f568fefdcacb49e5fa18b010bd73707e
Sonal Mittal <sonalmittal140103@gmail.com>
2025-03-02 23:54:00 +0530

    while loop

---
+++ Loop Control/while_value.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// int main(){
+//     int i=0;
+//     while(i<10){
+//         printf("The value of i : %d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+int main() {
+    int i=0;
+    while(i<=20){
+        if(i>=10){
+            printf("The value of i is %d\n" ,i);
+        }
+        i++;
+    }
+}
