4d6d7f723417cc22229b093cb9db9e20c79c7d9f
Sonal Mittal <sonalmittal140103@gmail.com>
2025-04-03 00:34:07 +0530

    Area of Square

---
+++ Chapter 1/areaOfSquare.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float side;
+    printf("Enter the side of the square : \n");
+    scanf("%f", &side);
+    float area ;
+    area = side * side;
+    printf("Area of the Square is %f", area);
+}
