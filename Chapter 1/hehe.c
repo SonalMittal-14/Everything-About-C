006a79856a75c56ebb2d1efb864e9b161d0b9c4d
Sonal Mittal <sonalmittal140103@gmail.com>
2025-04-03 00:22:56 +0530

    Just some basis

--- Chapter 1/hehe.c
+++ Chapter 1/hehe.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
     int age = 22;
-    printf("My age is %d.",age);
+    printf("My age is %d.\n",age);
+    
+    float pi = 3.14;
+    printf("the value of pi is %f.\n", pi);
+
+    char name = 's';
+    printf("my name start with %c", name);
     return 0;
+
 }
