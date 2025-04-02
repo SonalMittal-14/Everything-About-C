0aec068ffa17244404b824206378da43b9789abc
Sonal Mittal <sonalmittal140103@gmail.com>
2025-04-03 00:28:53 +0530

    Adding two numbers

---
+++ Chapter 1/adding.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("Enter the first number : \n");
+    scanf("%d", &a);
+    printf("Enter the second number : \n");
+    scanf("%d", &b);
+    int c;
+    c = a +b;
+    printf("The sum is : %d\n",c);
+    return 0;
+}
